8b9fe0f5aed30d9ae9ff737b9025977e3b1073d6
IpShot <zhernosek.roman@gmail.com>
2022-02-14 11:49:56 +0000

    export TipContainer and coordinates utils

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ export { Highlight } from "./components/Highlight";
 export { Popup } from "./components/Popup";
 export { AreaHighlight } from "./components/AreaHighlight";
 export { PdfLoader } from "./components/PdfLoader";
+export { TipContainer } from "./components/TipContainer";
 
 export * from "./types";
 export * from "./constants";
+export * from "./lib/coordinates";
