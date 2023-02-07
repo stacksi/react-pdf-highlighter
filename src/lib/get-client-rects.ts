4b6ba4d5ff34cbb8ae569c513a5fb137d69bbda9
IpShot <zhernosek.roman@gmail.com>
2023-02-07 13:11:08 +0000

    filter bad rects in get client rects stacksi/app#1491

--- src/lib/get-client-rects.ts
+++ src/lib/get-client-rects.ts
@@ -1,6 +1,7 @@
 import type { LTWHP, Page } from "../types.js";
 
 import optimizeClientRects from "./optimize-client-rects";
+import { getPageFromElement, isHTMLElement } from './pdfjs-dom.js';
 
 const isClientRectInsidePageRect = (clientRect: DOMRect, pageRect: DOMRect) => {
   if (clientRect.top < pageRect.top) {
@@ -24,7 +25,35 @@ const getPageBorders = (page: Page) => {
   return {
     borderTop: parseFloat(style.borderTopWidth.replace('px', '')),
     borderLeft: parseFloat(style.borderLeftWidth.replace('px', '')),
+    borderBottom: parseFloat(style.borderBottomWidth.replace('px', '')),
+    borderRight: parseFloat(style.borderRightWidth.replace('px', '')),
   }
+}
+
+const getLimitsY = (range: Range) => {
+  const limits = {
+    start: { y: 0, page: -1 },
+    end: { y: 99999, page: -1 }
+  }
+
+  const startEl = range.startContainer.parentElement
+  const endEl = range.endContainer.parentElement
+
+  if (isHTMLElement(startEl)) {
+    limits.start = {
+      y: startEl!.offsetTop - startEl!.offsetHeight,
+      page: getPageFromElement(startEl!)?.number || -1
+    }
+  }
+
+  if (isHTMLElement(endEl)) {
+    limits.end = {
+      y: endEl!.offsetTop + endEl!.offsetHeight,
+      page: getPageFromElement(endEl!)?.number || -1
+    }
+  }
+
+  return limits
 }
 
 const getClientRects = (
@@ -32,13 +61,14 @@ const getClientRects = (
   pages: Page[],
   shouldOptimize: boolean = true
 ): Array<LTWHP> => {
+  const limits = getLimitsY(range)
   const clientRects = Array.from(range.getClientRects());
-
   const rects: LTWHP[] = [];
 
   for (const clientRect of clientRects) {
     for (const page of pages) {
       const pageRect = page.node.getBoundingClientRect();
+      const { borderTop, borderBottom, borderLeft, borderRight } = getPageBorders(page);
 
       if (
         isClientRectInsidePageRect(clientRect, pageRect) &&
@@ -46,11 +76,10 @@ const getClientRects = (
         clientRect.bottom >= 0 &&
         clientRect.width > 0 &&
         clientRect.height > 0 &&
-        clientRect.width < pageRect.width &&
-        clientRect.height < pageRect.height
+        clientRect.width < pageRect.width - borderLeft - borderRight &&
+        clientRect.height < pageRect.height - borderTop - borderBottom
       ) {
-        const { borderTop, borderLeft } = getPageBorders(page);
-        const highlightedRect = {
+        const rect = {
           top: clientRect.top + page.node.scrollTop - pageRect.top - borderTop,
           left: clientRect.left + page.node.scrollLeft - pageRect.left - borderLeft,
           width: clientRect.width,
@@ -58,7 +87,14 @@ const getClientRects = (
           pageNumber: page.number,
         } as LTWHP;
 
-        rects.push(highlightedRect);
+        if (rect.top >= borderTop && rect.left >= borderLeft
+          && (page.number === limits.start.page && rect.top >= limits.start.y
+            || page.number !== limits.start.page)
+          && (page.number === limits.end.page && rect.top <= limits.end.y
+            || page.number !== limits.end.page)
+        ) {
+          rects.push(rect);
+        }
       }
     }
   }
